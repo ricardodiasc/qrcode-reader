5052f747ebfb02241ed2922371e0005eaa492201
Ricardo Dias Cavalcante <ricardodc@gmail.com>
2020-04-12 04:53:26 -0300

    Adding build folder

---
+++ precache-manifest.a6098b43cd019e56f296bb1ccea25642.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a5206301efe3d212f16c4a681c859f4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c6df7c7788dccab54397",
+    "url": "./static/css/main.0099894f.chunk.css"
+  },
+  {
+    "revision": "aaa6135dcb2497787ff8",
+    "url": "./static/js/2.d4cfbe18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d4cfbe18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6df7c7788dccab54397",
+    "url": "./static/js/main.1658b5ac.chunk.js"
+  },
+  {
+    "revision": "7b20f5dc2e8ee890439b",
+    "url": "./static/js/runtime-main.ed9dae3f.js"
+  }
+]);
